937e5c1076657a5e8b0df920d6321316e8815342
rebornrival <renee@devivo.com>
2024-10-17 16:31:55 -0700

    added checkerboard pt 3, working on customization UI

--- game jam/button.gd
+++ game jam/button.gd
@@ -13,5 +13,5 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/typing.tscn")
+	get_tree().change_scene_to_file("res://characterCustomization.tscn")
 	pass # Replace with function body.
