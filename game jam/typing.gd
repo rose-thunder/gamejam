cb9d3f09717ed81c84e5ecb1dc34bb7ad3de7cd3
DweedleDeBeetle <cassidyroberts074@gmail.com>
2024-10-16 17:31:37 -0700

    Updated code

--- game jam/typing.gd
+++ game jam/typing.gd
@@ -1,16 +1,35 @@
 extends Node2D
 var rizz = 50
-var current_text = " It's a me, a Mario\n Oh a no, I a lost me pizza :("
+var text_list = ["Hello Mario.","Howdy RC. How's it going?",
+	"Swimmingly, thanks for asking... So, tell me about yourself. I see you're a student as well, what's your major?",
+	"I'm multiclassing in Game Design and CompSci at the University of Arizona. Bear Down!"]
+var text_index = 0
+var typeable = false
 
 func _ready():
-	$backdrop.text = current_text
 	pass 
 
 func _process(delta):
-	var index = $typed.text.length() - 1
-	if $typed.text[index] != $backdrop.text[index]:
+	var current_text = text_list[text_index]
+	$backdrop.text = current_text
+	$rizz.text = "Rizz: "+str(rizz)
+	
+	if Input.is_action_just_pressed("enter") and typeable == false:
+		text_index += 1
+	if $typed.text == $backdrop.text and typeable == true:
+		text_index += 1
+	
+	if text_index % 2 == 0:
+		$typed.visible = false
+		typeable = false
+	elif text_index % 2 != 0:
+		$typed.visible = true
+		typeable = true
+	
+	var index = $typed.text.length() -1
+	if index >=0 and $typed.text[index] != $backdrop.text[index] and typeable == true and $typed.text.length() >= 0:
 		rizz-=1
 		$typed.backspace()
 		$AnimationPlayer.play("shake")
-	$rizz.text = "Rizz: "+str(rizz)
+	
 	pass
