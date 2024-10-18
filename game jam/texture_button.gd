e98b84bd221d02c57c907fa73d3423be503d4c12
rebornrival <renee@devivo.com>
2024-10-18 16:20:13 -0700

    working on typing scene

--- game jam/texture_button.gd
+++ game jam/texture_button.gd
@@ -5,13 +5,21 @@ extends TextureButton
 func _ready() -> void:
 	pass # Replace with function body.
 
+@onready var rc_1: Sprite2D = $"../RC1"
+@onready var rc_2: Sprite2D = $"../RC2"
+@onready var rc_3: Sprite2D = $"../RC3"
+@onready var rc_4: Sprite2D = $"../RC4"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+@export var sprite: Sprite2D
+var rc1 = load("res://assets/RC/rc overalls skin tone 4.png")
 
-func _on_pressed() -> void:
-	var sprite = $RC1
+#func _on_pressed() -> void:
 	
-	$RC1.texture = load("res://assets/RC/rc overalls skin tone 4.png")
+	#sprite.texture = rc_1
+	
+	
+	
