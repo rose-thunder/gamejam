331d8ca7f33e3f0813163e9b905bfbfc362cafd9
rebornrival <renee@devivo.com>
2024-10-19 13:59:23 -0700

    added hair to customization

--- game jam/character_customization.gd
+++ game jam/character_customization.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 #var skinList = [$RCSkin.play("RC1"), $RCSkin.play("RC2"), $RCSkin.play("RC3"), $RCSkin.play("RC4")]
 var index = 0
+var index1 = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +20,15 @@ func _process(delta: float) -> void:
 		$RCSkin.play("RC3")
 	elif index == 3:
 		$RCSkin.play("RC4")
+		
+	if index1 == 0:
+		$RCHair.play("Cassidy")
+	elif index1 == 1:
+		$RCHair.play("Elli")
+	elif index1 == 2:
+		$RCHair.play("Jack")
+	elif index1 == 3:
+		$RCHair.play("Renee")
 	pass
 
 
@@ -38,4 +48,20 @@ func _on_right_pressed() -> void:
 	pass
 
 func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_left_1_pressed() -> void:
+	if index1 - 1 == -1:
+		index1 = 3
+	else:
+		index1 -= 1
+	pass # Replace with function body.
+
+
+func _on_right_1_pressed() -> void:
+	if index1 + 1 == 4:
+		index1 = 0
+	else:
+		index1 += 1
 	pass # Replace with function body.
