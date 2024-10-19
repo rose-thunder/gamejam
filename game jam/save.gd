141c4e3a08b0bee713b0a3de0b4353a79e454b75
Elli Cano <melongem64@gmail.com>
2024-10-18 19:30:29 -0700

    progress on save script

--- game jam/save.gd
+++ game jam/save.gd
@@ -1,9 +1,17 @@
 class_name Save
 extends Node
-@onready var player = $
+
+@onready var rc1 = $RC1
+@onready var rc2 = $RC2
+@onready var rc3 = $RC3
+@onready var rc4 = $RC4
+
+var skin = [rc1, rc2, rc3, rc4]
 
 func save_game():
 	var file = FileAccess.open("res://savegame.data", FileAccess.WRITE)
+	
+	var saved_data = {}
 	
 	pass
 
